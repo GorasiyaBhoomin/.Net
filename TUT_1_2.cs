74fc6bcab999dcbe64ea4cfbdffecfcbf33b79ba
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-06 13:42:39 +0530

    Add project files.

---
+++ TUT_1_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TUT_1_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:BHOOMIN GORASIYA");
+            Console.WriteLine("DOB:06/02/2005");
+            Console.WriteLine("Address:Near SBI Bank,Mankuva,Bhuj-Kutch,Gujarat");
+            Console.WriteLine("City:Bhuj");
+            Console.WriteLine("Pincode:370030");
+            Console.WriteLine("State: Gujarat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: gorasiyabhoomin8@gmail.com");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TUT_1_3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TUT_1_3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is Even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is Odd.");
+
+            }
+        }
+    }
+}
