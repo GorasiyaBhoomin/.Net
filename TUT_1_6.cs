509495559593188a9c2317e221a7891368cae3e4
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-06 14:02:03 +0530

    modified

--- TUT_1_6.cs
+++ TUT_1_6.cs
@@ -9,20 +9,19 @@ namespace ConsoleApplication1
             int a, b, c, result;
 
             Console.Write("Enter Number 1: ");
-            string str = Console.ReadLine(); // ✅ Missing statement 1
+            string str = Console.ReadLine();
             a = Convert.ToInt32(str);
 
             Console.Write("Enter Number 2: ");
-            str = Console.ReadLine(); // ✅ Missing statement 2
+            str = Console.ReadLine(); 
             b = Convert.ToInt32(str);
 
             Console.Write("Enter Number 3: ");
             str = Console.ReadLine();
-            c = Convert.ToInt32(str); // ✅ Missing statement 3
+            c = Convert.ToInt32(str); 
 
             result = Sum(a, b, c);
-            Console.WriteLine("Sum : {0}", result); // ✅ Missing statement 4
-
+            Console.WriteLine("Sum : {0}", result); 
             Console.Read();
         }
 
