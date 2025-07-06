bf70e89e172f9dd66abfef2afeec065057b98cda
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-06 14:10:27 +0530

    committed

---
+++ TUT_1_8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NameToUpper
+{
+    class TUT_1_8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine(); // Input: John F Kennedy
+
+            string upperName = name.ToUpper(); // Convert to uppercase
+
+            Console.WriteLine("Output: " + upperName); // Output: JOHN F KENNEDY
+
+            Console.ReadLine(); // Pause the program
+        }
+    }
+}
