37ecba648a6442b2ca91dca5fc68c9193a485fed
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-07 21:04:54 +0530

    commited

---
+++ TUT_1_10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaskMobileNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your mobile number: ");
+            string mobile = Console.ReadLine();
+
+            if (mobile.Length >= 5)
+            {
+                string masked = mobile.Substring(0, mobile.Length - 5) + "XXXXX";
+                Console.WriteLine("Output: " + masked);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Mobile number must have at least 5 digits.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
