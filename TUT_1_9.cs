3af36bb6b4895045d99dbfa43b076458efe04743
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-07 20:58:24 +0530

    commited

---
+++ TUT_1_9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ToggleCaseApp
+{
+    class TUT_1_9
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string input = Console.ReadLine(); 
+
+            string toggleCase = "";
+
+            foreach (char ch in input)
+            {
+                if (char.IsUpper(ch))
+                    toggleCase += char.ToLower(ch);
+                else if (char.IsLower(ch))
+                    toggleCase += char.ToUpper(ch);
+                else
+                    toggleCase += ch;
+            }
+
+            Console.WriteLine("Output: " + toggleCase); 
+
+            Console.ReadLine();
+        }
+    }
+}
