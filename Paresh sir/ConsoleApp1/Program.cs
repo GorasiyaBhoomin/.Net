ddc8f799e4f0ccdbd0ef1ce32f273895743b0281
Bhoomin Gorasiya <gorasiyabhoomin8@gmail.com>
2025-07-11 13:31:08 +0530

    Add files via upload

---
+++ Paresh sir/ConsoleApp1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 5; i++)       //Pattern Program-1
+            //{
+            //    for (int j = 0; j <= 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //******
+            //******
+            //******
+            //******
+            //******
+
+            //for (int i = 1; i <= 5; i++)         //Pattern Program-2
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //*
+            //**
+            //***
+            //****
+            //*****
+
+            //for (int i = 1; i <= 5; i++)          //Pattern Program-3
+            //{
+            //    for (int j = i; j <= 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            //*****
+            //****
+            //***
+            //**
+            //*
+
+            //for (int i = 1; i <= 5; i++)         //Pattern Program-4
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //1
+            //12
+            //123
+            //1234
+            //12345
+
+
+            //for (int i = 1; i <= 9; i++)         //Pattern Program-5
+            //{
+            //    int k = i <= 5 ? i : 10-i;
+            //    for (int j = 1; j <= k; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //*****
+            //****
+            //***
+            //**
+            //*
+
+            //for (int i = 1; i <= 5; i++)         //Pattern Program-6
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        for (int j = 1; j <= 10; j++)
+            //        {
+            //            {
+            //                int k = i + j;
+            //                Console.Write(i + "*" + j + "=" + k);
+            //            }
+            //            Console.WriteLine();
+            //        }
+            //    }
+
+
+            //int a = 5;
+            //int b = 5;
+            //int res = a++ + b++ + b++ + a++ + ++a + ++b + a++;
+            //Console.WriteLine(res);
+
+
+
+        }
+    }
+}
