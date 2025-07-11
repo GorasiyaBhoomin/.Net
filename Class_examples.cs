6037504a528c9db92c83afe5137f19776ef2a04f
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-11 12:59:00 +0530

    commited

---
+++ Class_examples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class class1
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            //index wise interation
+            for(int i = 0; i < a.Length;i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            foreach(int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
