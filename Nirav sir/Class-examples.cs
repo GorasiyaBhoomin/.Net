415166a94c44d1d1dee23ff88b804706d3cdc8e7
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-15 11:25:51 +0530

    commited

--- Nirav sir/Class-examples.cs
+++ Nirav sir/Class-examples.cs
@@ -6,7 +6,7 @@ namespace Praticals
     internal class demoArray1
     {
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             //P1
@@ -115,31 +115,125 @@ namespace Praticals
 
 
             //P9
-            int[] ary = new int[5];
-            bool[] freq = new bool[ary.Length];
-            //initilization of array elements
-            Console.WriteLine("enter array elements:");
-            for (int i = 0; i < ary.Length; i++)
-            {
-                ary[i] = Int32.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < ary.Length; i++)
-            {
-                if (freq[i] == true)
-                    continue;
+            //int[] ary = new int[5];
+            //bool[] freq = new bool[ary.Length];
+            ////initilization of array elements
+            //Console.WriteLine("enter array elements:");
+            //for (int i = 0; i < ary.Length; i++)
+            //{
+            //    ary[i] = Int32.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < ary.Length; i++)
+            //{
+            //    if (freq[i] == true)
+            //        continue;
 
-                int cnt = 1;
-                for (int j = i + 1; j < ary.Length; j++)
+            //    int cnt = 1;
+            //    for (int j = i + 1; j < ary.Length; j++)
+            //    {
+            //        if (ary[i] == ary[j])
+            //        {
+            //            freq[j] = true; //mark as counted
+            //            cnt++;
+            //        }
+
+            //    }
+            //    Console.WriteLine("frequecy of (0) is (1) time:", ary[i], cnt);
+            //}
+
+            //Multi-Dimensional array
+            //collection of rows and columns
+
+            //int[,] ary = new int[2, 2];
+            //ary[0, 0] = 1;
+            //ary[0, 2] = 2;
+            //ary[1, 0] = 3;
+            //ary[2, 0] = 4;
+            //ary[1, 1] = 5;
+
+            //Console.WriteLine("First Element;", ary[0, 0]);
+
+
+            //int[,] ary = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            //for (int i = 0; i < ary.GetLength(0); i++)
+            //{
+
+            //    for (int j = 0; j < ary.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(ary[i, j]);
+            //    }
+
+            //}
+
+
+            //int[,] = ary = new int[3, 3];
+            //Console.WriteLine("Enter elments of 3*3 array:");
+            //for (int i = 0;i<ary.GetLength(0);i++)
+            //{
+            //    for (int j = 0; j < ary.GetLength(1); j++)
+            //    {
+            //        ary[i,j] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+            //for (int i = 0; i < ary.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < ary.GetLength(1); j++)
+            //    {
+            //        Console .Write(args[i,j]);
+            //    }
+            //}
+
+
+
+            ////addition of two matrices:
+            ////=> declare ary1
+            ////=>Initialize ary1through ReadlIne()
+            ////=>declare ary2
+            ////=>Initialize ary2 through ReadLine()
+            ////=> declare sum(demoArray1 of same size)
+            ////=> nested loop => perform addition of ary1 and ary2
+            ////=>inside nested loop display sum array
+
+            //int[,] bry= new int[2,2];
+
+            //int[,] cry = new int[2, 2];
+
+            //for (int i = 0;i<cry.GetLength(0); int++)
+            //{
+            //    for (int j = 0; j < cry.GetLength(1); int++)
+            //    {
+            //        cry[i,j] = args[i, j] + bry[i,j];
+            //    }
+
+            //}
+
+
+
+            int[][] jAry = new int[4][];
+            jAry[0] = new int[4];
+            jAry[1] = new int[3];
+            jAry[2] = new int[2];
+            jAry[1] = new int[1];
+            Console.WriteLine("write jagged array emlements:");
+            for (int i = 0; i < jAry.Length; i++)
+            {
+                for (int j = 0; j < jAry[i].Length; j++)
                 {
-                    if (ary[i] == ary[j])
-                    {
-                        freq[j] = true; //mark as counted
-                        cnt++;
-                    }
-
+                    jAry[i][j] = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.WriteLine("frequecy of (0) is (1) time:", ary[i], cnt);
             }
+            Console.WriteLine("Jagged Array is:");
+            for (int i = 0; i < jAry.Length; i++)
+            {
+                for (int j = 0; j < jAry[i].Length; j++)
+                {
+                    Console.Write(jAry[i][j]);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
+    
 }
---
+++ Nirav sir/jag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array1
+{
+    internal class jag
+    {
+        public static void Main(string[] args)
+        {
+            int[][] jAry = new int[4][];
+            jAry[0] = new int[4];
+            jAry[1] = new int[3];
+            jAry[2] = new int[2];c
+            jAry[3] = new int[1]; 
+
+            Console.WriteLine("Write jagged array elements:");
+            for (int i = 0; i < jAry.Length; i++)
+            {
+                for (int j = 0; j < jAry[i].Length; j++)
+                {
+                    Console.Write($"Element [{i}][{j}]: ");
+                    jAry[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nJagged Array is:");
+            for (int i = 0; i < jAry.Length; i++)
+            {
+                for (int j = 0; j < jAry[i].Length; j++)
+                {
+                    Console.Write(jAry[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
