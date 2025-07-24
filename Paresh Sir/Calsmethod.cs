65776f2915fbbadca5c897d4443bba2d1949cd7b
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-07-24 10:48:17 +0530

    commit

--- Paresh Sir/Calsmethod.cs
+++ Paresh Sir/Calsmethod.cs
@@ -8,11 +8,36 @@ namespace Calsmethod
 {
      class Calsmethod
     {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        static int Div(int a, int b)
+        {
+            return a / b;
+        }
         static void Main(string[] args)
         {
-            int test;
-            static int test2;
-             int Add
+            int a=20,b=20;
+            int ra;
+            ra = Add(a, b);
+            Console.WriteLine("Addition:" + ra);
+            ra = Sub(a, b);
+            Console.WriteLine("Substaction:" + ra);
+            ra = Mul(a, b);
+            Console.WriteLine("Multiplication:" + ra);
+            ra = Div(a, b);
+            Console.WriteLine("Division:" + ra);
+
+
 
         }
     }
---
+++ TUT_1_13.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class TUT_1_13
+{
+    static void Main()
+    {
+        int n = 11; // Number of terms to print
+        int a = 0, b = 1, c;
+        Console.Write("Fibonacci Series: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
