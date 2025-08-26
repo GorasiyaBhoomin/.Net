68a86cdc2dcf7abcde62fa03b36aa84ec48eebc9
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-08-26 10:59:06 +0530

    commit

--- Nirav sir/Propertdemo.cs
+++ Nirav sir/Propertdemo.cs
@@ -4,34 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace .Net
 {
-    public class Properdemo
+    public class Demo1
     {
         int x; //private member od class
         int y;
 
+        public Demo1(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
         public int X
         {
             set { x = value; }
-            get { return x; }   //read write property
+            //get { return x; }   //read write property
         }
         public int Y
         {
             set { y = value; }
-            get { return y; }
+            //get { return y; }
         }
     }
-    internal class Properdemo
+    internal class Propertdemo
     {
         static void Main(string[] args)
         {
-            Demo1 ob = new Demo1();
-            ob.X = 100;
+            Demo1 ob = new Demo1(1000, 200);
+            //ob.X = 50;
             Console.WriteLine("value of private x:" + ob.X);
-            ob.Y = 200;
+            //ob.Y = 150;
             Console.WriteLine("value of private x:" + ob.Y);
-            Console.WriteLine("value of private x:" + ob.X + ob.Y);
+            Console.WriteLine("value of private x and y:" + (ob.X + ob.Y));
         }
     }
 }
@@ -45,3 +49,6 @@ namespace ConsoleApp3
 //intermediate - only to transfer the data
 //set: property is IServiceProvider used to set values
 //get: property is used to retrive the values
+
+
+
