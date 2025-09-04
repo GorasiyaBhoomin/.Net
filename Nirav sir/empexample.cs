6055424511bf519ff38b0fb3295b4e5e374c9d99
GorasiyaBhoomin <gorasiyabhoomin@gmail.com>
2025-09-04 09:40:49 +0530

    commit

---
+++ Nirav sir/empexample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array1
+{
+    public class demo2
+    {
+        int empcode;
+        string eName;
+        string designation;
+        string instname;
+        float basicpay;
+        //public void getData(int empId, string eName, double salary)
+        //{
+        //    this.empId = empId;
+        //    this.eName = eName;
+        //    this.salary = salary;
+
+        //}
+        public void showData()
+        {
+            Console.WriteLine("empcode:" + empcode);
+            Console.WriteLine("eName:" + eName);
+            Console.WriteLine("designation:" + designation);
+            Console.WriteLine("instname:" + instname);
+            Console.WriteLine("basicpay:" + basicpay);
+
+
+        }
+
+
+
+    }
+    internal class staticdemo
+    {
+
+        static void Main(string[] args)
+        {
+            employee emp= new employee();
+            emp.showData();
+
+        }
+    }
+}
+
---
+++ Nirav sir/staticdemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array1
+{
+    public class demo2
+    {
+
+        int x;
+        static int y = 20;
+        public demo2()
+        {
+        }
+        public demo2(int x)
+        {
+            this.x = x;
+        }
+
+        public static int addnum()
+        {
+            demo2 ob = new demo2(30);
+            return (ob.x + y);
+        }
+
+        public void show()
+        {
+            Console.WriteLine("x is:" + x);
+            Console.WriteLine("y is:" + y);
+        }
+
+    }
+    internal class staticdemo
+    {
+
+        static void Main(string[] args)
+        {
+            demo2 ob = new demo2(56);
+            Console.WriteLine("Addition is:" + demo2.addnum());
+            ob.show();
+        }
+    }
+}
